f31581220958bf520f0b7c0f7b4810b6f90ef16d
CorentinTrt <corentintruffaut@gmail.com>
2022-11-25 15:18:32 -0500

    models/user.go - add data validation on simple types

--- service-users-data/internals/database/models/user.go
+++ service-users-data/internals/database/models/user.go
@@ -5,30 +5,31 @@ import (
 	"encoding/json"
 	"io"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/go-playground/validator"
 )
 
 // User type define user object that will be stored in the DB
 type User struct {
-	ID                primitive.ObjectID `bson:"_id" json:"_id"`
-	FirstName         string             `bson:"first_name" json:"firstName"`
-	MiddleNames       []string           `bson:"middle_names" json:"middleNames"`
-	LastName          string             `bson:"last_name" json:"lastName"`
-	Adress            adress             `bson:"adress" json:"adress"`
-	Salary            salary             `bson:"salary" json:"salary"`
-	Job               string             `bson:"job" json:"job"`
-	JoStatus          string             `bson:"job_status" json:"jobStatus"`
-	BeginingDate      int                `bson:"begining_date" json:"beginingDate"`
-	NextInterviewDate int                `bson:"next_interview_date" json:"nextInterviewDate"`
-	LastInterviewDate int                `bson:"last_interview_date" json:"lastInterViewDate"`
-	ActivityStatus    string             `bson:"activity_status" json:"activityStatus"`
-	CreatedOn         int                `bson:"created_on" json:"-"`
-	UpdatedOn         int                `bson:"updated_on" json:"-"`
-	DeletedOn         int                `bson:"deleted_on" json:"-"`
+	FirstName         string   `bson:"first_name" json:"firstName" validate:"required,max=50"`
+	MiddleNames       []string `bson:"middle_names" json:"middleNames"`
+	LastName          string   `bson:"last_name" json:"lastName" validate:"required,max=50"`
+	Age               uint8    `bson:"age" json:"age" validate:"gte=16,lte=99"`
+	Email             string   `bson:"email" json:"email" validate:"required,email"`
+	Adress            adress   `bson:"adress" json:"adress"`
+	Salary            salary   `bson:"salary" json:"salary"`
+	Job               string   `bson:"job" json:"job" validate:"max=50"`
+	JoStatus          string   `bson:"job_status" json:"jobStatus" validate:"omitempty,oneof=Intern Extern"`
+	BeginingDate      int      `bson:"begining_date" json:"beginingDate"`
+	NextInterviewDate int      `bson:"next_interview_date" json:"nextInterviewDate"`
+	LastInterviewDate int      `bson:"last_interview_date" json:"lastInterViewDate"`
+	ActivityStatus    string   `bson:"activity_status" json:"activityStatus" validate:"omitempty,oneof=active inactive"`
+	CreatedOn         int      `bson:"created_on" json:"-"`
+	UpdatedOn         int      `bson:"updated_on" json:"-"`
+	DeletedOn         int      `bson:"deleted_on" json:"-"`
 }
 
 type adress struct {
-	Number   int    `bson:"number" json:"number"`
+	Number   uint16 `bson:"number" json:"number"`
 	Street   string `bson:"street" json:"street"`
 	City     string `bson:"city" json:"city"`
 	Province string `bson:"province" json:"province"`
@@ -42,12 +43,25 @@ type salary struct {
 // Users type is a slice of User
 type Users []*User
 
-// ToJSON serialize and return users data
+// Validate the User
+func (u *User) Validate() error {
+	v := validator.New()
+	return v.Struct(u)
+}
+
+// ToJSON serialize and return a list of users
 func (u *Users) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(u)
 }
 
+// ToJSON serialize and return user data
+func (u *User) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(u)
+}
+
+// FromJSON deserialize and return users data
 func (u *User) FromJSON(r io.Reader) error {
 	e := json.NewDecoder(r)
 	return e.Decode(u)
